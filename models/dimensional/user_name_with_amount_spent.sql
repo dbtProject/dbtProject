b84889397b48d3b19b02305a5429becd5c8994d2
dbtproject0@gmail.com <dbtproject0@gmail.com>
2023-01-24 16:45:44 +0000

    followup commit

--- models/dimensional/user_name_with_amount_spent.sql
+++ models/dimensional/user_name_with_amount_spent.sql
@@ -1,4 +1,4 @@
-select t.user_id, first_name, last_name, t.total_amount from {{ ref('stg_customers') }} as c
-inner join {{ ref('total_amount_spent_per_order') }} as t
-on c.user_id = t.user_id
-order by t.user_id
+select c.user_id, first_name, last_name, total_amount
+from {{ ref('total_amount_spent_per_order') }} as t, {{ ref('stg_customers') }} as c
+where t.user_id = c.user_id
+order by c.user_id
--- models/inter/user_joined_to_amount_spent_per_order.sql
+++ models/inter/user_joined_to_amount_spent_per_order.sql
@@ -1,5 +1,4 @@
-with tmp as (select user_id, order_id from {{ ref("stg_orders") }})
-select o.order_id, user_id, amount_spent
-from {{ ref("amount_spent_per_order") }} as o
-inner join tmp on tmp.order_id = o.order_id
-order by o.order_id
+select a.order_id, user_id, amount_spent
+from {{ ref('stg_orders') }} as o, {{ ref('amount_spent_per_order') }} as a
+where a.order_id = o.order_id
+order by a.order_id
