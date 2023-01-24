d879bf6684af5a0436c4b7332d38f0c103b903c2
dbtproject0@gmail.com <dbtproject0@gmail.com>
2023-01-24 16:58:34 +0000

    followup commit

--- models/dimensional/dim_summary.sql
+++ models/dimensional/dim_summary.sql
@@ -1,4 +1,4 @@
 select c.user_id, first_name, last_name, total_amount
 from {{ ref('fact_total_amount') }} as t, {{ ref('stg_customers') }} as c
-where t.user_id = c.user_id
+where c.user_id = t.user_id
 order by c.user_id
--- models/inter/inter_user_amount.sql
+++ models/inter/inter_user_amount.sql
@@ -1,4 +1,4 @@
 select a.order_id, user_id, amount_spent
 from {{ ref('stg_orders') }} as o, {{ ref('inter_order_amount') }} as a
-where a.order_id = o.order_id
+where o.order_id = a.order_id
 order by a.order_id
